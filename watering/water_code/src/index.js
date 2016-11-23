e274282fa5715fd7f60720ed02ddb5c57c515d7c
Soros Liu <soros.liu1029@gmail.com>
2016-11-23 17:31:41 +0800

    enable soil moisture sensor to work

--- watering/water_code/src/index.js
+++ watering/water_code/src/index.js
@@ -1,15 +1,33 @@
 'use strict';
 
 // soil moisture sensor
-var moisture_sensor;
+var moistureSensor;
+// oled display
+var oled;
 
 $.ready(function (error) {
     if (error) {
         console.log(error);
         return;
     }
+    moistureSensor = $('#soil-moisture-sensor');
+    oled = $('#oled');
+    oled.print(0, 0, 'Start');
 
-    $('#oled').print(0, 0, 'Hello Ruff');
+    moistureSensor.on('veryWet', function() {
+        console.log('Soil is very wet');
+        oled.print(0, 0, 'very wet');
+    });
+
+    moistureSensor.on('wet', function() {
+        console.log('Soil is wet');
+        oled.print(0, 0, 'wet');
+    });
+
+    moistureSensor.on('dry', function() {
+        console.log('Soil is dry');
+        oled.print(0, 0, 'dry');
+    });
 });
 
 $.end(function () {
