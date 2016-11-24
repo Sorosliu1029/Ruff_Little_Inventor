5eab139825a526fdffef9bb56a0e9885a94078ce
Soros Liu <soros.liu1029@gmail.com>
2016-11-24 10:38:45 +0800

    enable relay to control water pump depending on dry or wet

--- watering/water_code/src/index.js
+++ watering/water_code/src/index.js
@@ -4,6 +4,8 @@
 var moistureSensor;
 // oled display
 var oled;
+// relay to control water pump
+var relay;
 
 $.ready(function (error) {
     if (error) {
@@ -12,24 +14,32 @@ $.ready(function (error) {
     }
     moistureSensor = $('#soil-moisture-sensor');
     oled = $('#oled');
+    relay = $('#relay');
+    oled.clear();
     oled.print(0, 0, 'Start');
 
     moistureSensor.on('veryWet', function() {
         console.log('Soil is very wet');
+        oled.clear();
         oled.print(0, 0, 'very wet');
+        relay.turnOff();
     });
 
     moistureSensor.on('wet', function() {
         console.log('Soil is wet');
+        oled.clear();
         oled.print(0, 0, 'wet');
     });
 
     moistureSensor.on('dry', function() {
         console.log('Soil is dry');
+        oled.clear();
         oled.print(0, 0, 'dry');
+        relay.turnOn();
     });
 });
 
 $.end(function () {
-    $('#oled').clear();
+    relay.turnOff();
+    oled.clear();
 });
