a35f5521092573efc28d30ba922c0b4078b84eaf
Soros Liu <soros.liu1029@gmail.com>
2016-11-25 13:10:12 +0800

    add water time record

--- watering/src/index.js
+++ watering/src/index.js
@@ -6,6 +6,8 @@ var moistureSensor;
 var oled;
 // relay to control water pump
 var relay;
+// date object to record
+var date;
 
 $.ready(function (error) {
     if (error) {
@@ -16,13 +18,19 @@ $.ready(function (error) {
     oled = $('#oled');
     relay = $('#relay');
     oled.clear();
-    oled.print(0, 0, 'Start');
+    oled.print(0, 0, 'Monitor starts');
 
     moistureSensor.on('veryWet', function() {
         console.log('Soil is very wet');
         oled.clear();
         oled.print(0, 0, 'very wet');
-        relay.turnOff();
+        relay.turnOff(function () {
+            // record this moment as watering record
+            date = new Date();
+            oled.print(0, 2, 'Last water time:')
+            oled.print(0, 4, date.toLocaleDateString());
+            oled.print(0, 6, date.toLocaleTimeString());
+        });
     });
 
     moistureSensor.on('wet', function() {
@@ -35,6 +43,7 @@ $.ready(function (error) {
         console.log('Soil is dry');
         oled.clear();
         oled.print(0, 0, 'dry');
+        oled.print(0, 2, 'watering...');
         relay.turnOn();
     });
 });
