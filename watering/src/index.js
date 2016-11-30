c3dc919765dfb21f2a3c9855e87655321949404d
Soros Liu <soros.liu1029@gmail.com>
2016-11-30 18:25:02 +0800

    enable button to control pump

--- watering/src/index.js
+++ watering/src/index.js
@@ -2,12 +2,21 @@
 
 // soil moisture sensor
 var moistureSensor;
+
 // oled display
 var oled;
+
 // relay to control water pump
 var relay;
+
 // date object to record
 var date;
+
+// button
+var button;
+
+// toggle water pump
+var pumpOn = 0;
 
 $.ready(function (error) {
     if (error) {
@@ -17,6 +26,7 @@ $.ready(function (error) {
     moistureSensor = $('#soil-moisture-sensor');
     oled = $('#oled');
     relay = $('#relay');
+    button = $('#button');
     oled.clear();
     oled.print(0, 0, 'Monitor starts');
 
@@ -37,6 +47,13 @@ $.ready(function (error) {
         console.log('Soil is wet');
         oled.clear();
         oled.print(0, 0, 'wet');
+        relay.turnOff(function () {
+            // record this moment as watering record
+            date = new Date();
+            oled.print(0, 2, 'Last water time:')
+            oled.print(0, 4, date.toLocaleDateString());
+            oled.print(0, 6, date.toLocaleTimeString());
+        });
     });
 
     moistureSensor.on('dry', function() {
@@ -46,6 +63,16 @@ $.ready(function (error) {
         oled.print(0, 2, 'watering...');
         relay.turnOn();
     });
+
+    button.on('push', function () {
+        console.log('Button pushed');
+        if (pumpOn === 0) {
+            relay.turnOn();
+        } else {
+            relay.turnOff();
+        }
+        pumpOn ^= 1;
+    })
 });
 
 $.end(function () {
